b9f6fa44b05fab35aabdd7fb23f846a08a4557a8
romanarellano <romanarellano@yahoo.com>
2015-01-21 11:44:52 -1000

    nested for loops to get 3 of 3 elements

--- MultiDimensionalArrays.js
+++ MultiDimensionalArrays.js
@@ -1,1 +1,45 @@
+module.exports = {
 
+  generate_1d : function (num,num1) {
+  var arr=[];
+    if(num===undefined){
+      for(var i=0;i<3;i++){
+
+        
+          arr[i]=Math.round(Math.random()); 
+
+        
+      }
+        return arr;
+    }
+   else if(arguments.length===1) {
+     if(typeof num === 'string'){
+        return [];
+     }
+      for(var k=0;k<num;k++)
+          arr[k]=Math.round(Math.random());
+        return  arr;
+      }
+
+      // else if(arguments)
+    },
+
+
+    generate_2d : function (num1) {
+     
+      var arr=[];
+      element=new Array(3);
+      for(var i=0;i<3;i++){
+    
+          for(var j=0;j<3;j++){
+              arr[j]=(Math.floor(Math.random()*2));
+                
+          }
+          element[i]=arr;
+      }
+          return element;
+
+
+    }
+  
+};
--- test/test-generator.js
+++ test/test-generator.js
@@ -1,1 +1,65 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+var mda = require('./../MultiDimensionalArrays.js'); 
+ 
+describe('MultiDimensionalArray', function() {
+  describe('Generate Function', function () {
+  it('should be defined as a Function', function() {
+    expect(mda.generate_1d).to.be.a('function');
+    expect(mda.generate_1d).to.be.instanceOf(Function);
 
+  });
+
+  it('should return a single array with 3 boolean values',
+  function(){
+    var result = mda.generate_1d();
+
+    result.should.be.an('array');
+    result.should.have.length(3);
+   });
+
+    it('should return an array with a length of depending on number in first argument',function(){
+
+      var result = mda.generate_1d(14);
+
+      result.should.be.an('array');
+      result.should.have.length(14);
+
+    });
+
+    it("should return an empty array if a string is passed in num",function(){
+
+       var result = mda.generate_1d("potato");
+       result.should.be.an('array');
+       result.should.have.length(0);
+
+    });
+
+    it("should return nested for loop with same num of elements within the element",function(){
+
+       
+
+        var result = mda.generate_2d();
+        result.should.be.an('array');
+        
+
+        
+        result.forEach(function(x){
+          x.forEach(function(y){
+              expect(y).to.satisfy(function(y){ return y < 2;
+              });
+          });
+
+      
+
+           
+        });
+      
+
+    });
+
+     
+    }); 
+
+}); 
