6d88b9862951a70d4bbeb4299bca3f3705072db8
romanarellano <romanarellano@yahoo.com>
2015-01-20 21:03:55 -1000

    array with 1 argument

--- MultiDimensionalArrays.js
+++ MultiDimensionalArrays.js
@@ -1,13 +1,23 @@
 module.exports = {
 
-  generator : function () {
+  generator : function (num) {
+  var arr=[];
+    if(num===undefined){
+      for(var i=0;i<3;i++){
 
-    return [[0,1,0],[1,1,0],[0,1,1]];
+        for(var j=0;j<3;j++){
+          arr[i]=Math.round(Math.random()); 
 
-}// describe the behavior of calling the MultiDimensionalArray.generate 
-  //method with no arguments, it should return an array (length3) each 
-  //containing arrays (length 3) with random boolean values. ex:
-  
+        }
+      }
+        return arr;
+    }
+   else if(arguments.length===1) {
+      for(var k=0;k<num;k++)
+          arr[k]=Math.round(Math.random());
+        return  arr;
+      }
+    } 
   // if no args are passed return a single array
   // with 3 random boolean values
 };
