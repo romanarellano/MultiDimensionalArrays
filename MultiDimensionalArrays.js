17b1a3dc0eb475a111aec9e741875b8e0d2c9bfc
romanarellano <romanarellano@yahoo.com>
2015-01-20 20:26:41 -1000

    return array length

---
+++ MultiDimensionalArrays.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  generator : function () {
+
+    return [[0,1,0],[1,1,0],[0,1,1]];
+
+}// describe the behavior of calling the MultiDimensionalArray.generate 
+  //method with no arguments, it should return an array (length3) each 
+  //containing arrays (length 3) with random boolean values. ex:
+  
+  // if no args are passed return a single array
+  // with 3 random boolean values
+};
